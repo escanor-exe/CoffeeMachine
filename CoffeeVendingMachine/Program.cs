8d59b6b1905107ae553819ce72e30172487a5546
Escanor <234011099+escanor-exe@users.noreply.github.com>
2026-09-22 23:59:07 +0530

    feat: create new console project for coffee vending machine

---
+++ CoffeeVendingMachine/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CoffeeVendingMachine
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
